a2a53ae2e1a25efc168d0bf605a96748067a216f
Guildenstern70 <alessiosaltarin@gmail.com>
2021-11-11 15:42:11 +0100

    Added xUnit Tests

---
+++ SmartBlazor.Tests/SiteUserTest.cs
@@ -0,0 +1,28 @@
+/**
+ * 
+ * Project SmartBlazor
+ * Copyright (C) 2021 Alessio Saltarin 'alessiosaltarin@gmail.com'
+ * This software is licensed under MIT License. See LICENSE.
+ * 
+ **/
+
+using FluentAssertions;
+using SmartBlazor.Service;
+using Xunit;
+
+namespace SmartBlazor.Tests
+{
+    public class SiteUserTest
+    {
+        [Fact]
+        public void InsertNewUserTest()
+        {
+            var _userService = new SiteUserService();
+            _userService.AddUser("pippo", "pluto");
+            var _userPippo = _userService.GetUser("pippo");
+
+            _userPippo.Should().NotBeNull();
+            _userPippo!.Password.Should().Be("pluto");
+        }
+    }
+}
